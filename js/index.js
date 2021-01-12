a33f05feac385a1154486f87cda4976ba0b9a6cd
Laura Laane <laura.laane@gmail.com>
2021-01-12 11:28:58 +0200

    added back to top arrow

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const burger = document.querySelector('.hamburger');
+const arrow = document.querySelector('.arrow');
+const rootElement = document.documentElement;
 
 navToggle.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
@@ -14,5 +16,18 @@ navLinks.forEach(link => {
 });
 
 AOS.init({
-  duration: 1200,
-})
+	duration: 1200,
+});
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+const scrollFunction = () => {
+	let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+	if (rootElement.scrollTop / scrollTotal > 0.8) {
+		arrow.classList.add('arrowShow');
+	} else {
+		arrow.classList.remove('arrowShow');
+	}
+};
